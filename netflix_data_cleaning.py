cc9f786dd9b2ce802c8649e29ca92e04784025ba
tongtaffle <tongta.atp@gmail.com>
2026-03-26 17:50:16 +0700

    Add suscess files and everythong done!

---
+++ netflix_data_cleaning.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 23:12:07 2026
+
+@author: Tongtaffle
+"""
+
+import pandas as pd
+
+import ast
+
+import chardet
+
+with open("titles.csv", 'rb') as f:
+    result = chardet.detect(f.read(100000))
+
+print(result)
+
+title = pd.read_csv("titles.csv", encoding='latin-1')
+credit = pd.read_csv("credits.csv", encoding= 'latin-1')
+
+# Overview data #
+title.info()
+title.head()
+
+credit.info()
+credit.head()
+
+# Handle Missing Values #
+print(' +++ Handle Missing Values+++ ')
+print("titles.csv")
+print(title.isnull().sum())
+# Drop important nissing
+title = title.dropna(subset='title')
+# fill text
+title["description"] = title["description"].fillna("No description")
+title["age_certification"] = title["age_certification"].fillna("Unknown")
+# logical fill
+title["seasons"] = title["seasons"].fillna(0)
+
+# numeric fill
+title['imdb_score'] = title['imdb_score'].fillna(title['imdb_score'].median())
+title['imdb_votes'] = title['imdb_votes'].fillna(0)
+title['tmdb_popularity'] = title['tmdb_popularity'].fillna(title['tmdb_popularity'].median())
+title['tmdb_score'] = title['tmdb_score'].fillna(title['tmdb_score'].median())
+print("credits.csv")
+print(credit.isnull().sum())
+# logical fill
+credit["character"] = credit["character"].fillna("Unknown")
+
+# Fix data type 
+title['release_year'] = title['release_year'].astype(int)
+title['imdb_score'] = title['imdb_score'].astype(float)
+title['imdb_votes'] = title['imdb_votes'].astype(float)
+title['tmdb_score'] = title['tmdb_score'].astype(float)
+title['tmdb_popularity'] = title['tmdb_popularity'].astype(float)
+
+# clean list column 
+title['genres'] = title['genres'].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
+title['production_countries'] = title['production_countries'].apply(lambda x: ast.literal_eval(x) if pd.notnull(x) else [])
+title = title.explode('genres')
+
+# remove duplicate 
+title = title.drop_duplicates(subset=['id'])
+credit = credit.drop_duplicates()
+
+# Clean Credits Data 
+actor = credit[credit['role'] == 'ACTOR']
+director = credit[credit['role'] == 'DIRECTOR']
+credit = credit.dropna(subset=['name'])
+
+# merge data 
+merged = title.merge(credit, on='id', how='left')
+
+# outlier dertection 
+title = title[title['imdb_score'] <= 10]
+title = title[title['tmdb_score'] <= 10]
+title = title[title['runtime'] > 0]
+
+# Standardize text 
+title['title'] = title['title'].str.strip()
+title['title'] = title['title'].str.lower()
+
+# New column
+title['content_age'] = 2026 - title['release_year']
+title['avg_score'] = (title['imdb_score'] + title['tmdb_score']) / 2
+
+
+# Export #
+title.to_csv("title_cleaned.csv", index=False)
+credit.to_csv("credit_cleaned.csv", index=False)
+
+
+
